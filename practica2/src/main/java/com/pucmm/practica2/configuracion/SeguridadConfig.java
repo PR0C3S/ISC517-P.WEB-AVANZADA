3d32818544c73ff9422f87775f7be8c26a3db724
NDavidDuranM <ndaviddm@gmail.com>
2021-10-11 10:48:56 -0400

    SeguridadConfig agregado + Mock.html update

---
+++ practica2/src/main/java/com/pucmm/practica2/configuracion/SeguridadConfig.java
@@ -0,0 +1,53 @@
+package com.pucmm.practica2.configuracion;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SeguridadConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //Marcando las reglas para permitir unicamente los usuarios
+        http
+                .authorizeRequests()
+                .antMatchers("/","/css/**", "/js/**", "/actuator/**", "/webjars/**").permitAll() //permitiendo llamadas a esas urls.
+                .antMatchers("/dbconsole/**").permitAll()
+                .antMatchers("/thymeleaf/**", "/api/**", "/jpa/**").permitAll()
+                .antMatchers("/api-docs/**", "/api-docs.yaml", "/swagger-ui.html", "/swagger-ui/**").permitAll() //para OpenApi
+                .antMatchers("/admin/").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/mocks").permitAll() //hasAnyRole("ADMIN", "USER")
+                .anyRequest().authenticated() //cualquier llamada debe ser validada
+                .and()
+                .formLogin()
+                .loginPage("/login") //indicando la ruta que estaremos utilizando.
+                .failureUrl("/login?error") //en caso de fallar puedo indicar otra pagina.
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
+}
